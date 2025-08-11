46da301537c8c07caefbb379eb9349a88940a3dd
Philip Juhlin <juhlinphilip@gmail.com>
2025-08-11 11:33:44 +0200

    example of overflow and truncation

--- week_01/memoryaddress.c
+++ week_01/memoryaddress.c
@@ -12,4 +12,13 @@ int main(void)
 	printf("address pointer: %p\n", p);
 	printf("value pointer: %i\n", *p);
 	printf("value number: %i\n", number);
+	//int overflow
+	*p *= 20000000000000;
+	printf("value pointer when multiplied by 20000000000000: %i\n", *p);
+	printf("p has integer overflow\n");
+	//truncation
+	*p = 3;
+	printf("3/2 equal: %i\n", *p/2);
+	printf("3/2 actually equals: %f\n", (float) *p/2);
+
 }	
